c360a0952be19d8ed1c3165672b29b0e734c0b3b
Max <mharris382@gmail.com>
2024-08-19 18:37:47 -0400

    added python tool to automatically clean up broken PCG spawns

---
+++ Python/CleanupPCG.py
@@ -0,0 +1,31 @@
+import unreal
+
+level = unreal.EditorLevelLibrary.get_editor_world()
+
+all_actors = unreal.GameplayStatics.get_all_actors_of_class(level, unreal.Actor)
+
+for actor in all_actors:
+    # Get all components attached to the actor
+    components = actor.get_components_by_class(unreal.ActorComponent)
+
+    # Check if the actor has a PCGComponent
+    has_pcg_component = any(isinstance(component, unreal.PCGComponent) for component in components)
+    
+    #for component in components:
+        #if isinstance(component, unreal.PCGComponent):
+            #print(f"Actor '{actor.get_name()}' has a PCGComponent.")
+            #component.generate(True)
+            #print(f"  Called generate() on PCGComponent of actor '{actor.get_name()}'.")
+    
+    if has_pcg_component:
+        print(f"Actor '{actor.get_name()}' has a PCGComponent.")
+        for component in components:
+            if component.component_has_tag("PCG Generated Component"):
+                parent_component = component.get_attach_parent()
+                if not parent_component:
+                    component.destroy_component(component)
+
+
+# # Optional: Select all actors that meet the criteria in the Editor
+# actors_to_select = [actor for actor in all_actors if any(isinstance(comp, unreal.PCGComponent) for comp in actor.get_components_by_class(unreal.ActorComponent))]
+# unreal.EditorLevelLibrary.set_selected_level_actors(actors_to_select)
